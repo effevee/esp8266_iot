3423f696505e515d6f4a82f459158b9afb27e693
effevee <effevee@gmail.com>
2023-10-17 15:13:24 +0200

    added code for IdeaSpark ESP8266 OLED Dev Board

---
+++ sourcecode/micropython/lib/config_ideaspark.py
@@ -0,0 +1,27 @@
+# Pins
+DHT11_GPIO = 2
+SCL_GPIO = 14
+SDA_GPIO = 12
+
+# OLED
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+
+# Localtime
+DOW = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+TIMEZONE = 1                    # UTC+1 hour
+DAYLIGHT_SAVING_TIME = True     # +1 hour between last sunday of March 2AM and last sunday of October 3AM
+
+# intervals (seconds)
+NTP_INTERVAL = 7200      # 2 hours
+DHT_INTERVAL = 5
+OLED_INTERVAL = 15
+
+# Debug
+DEBUG = True
+
+# WLAN credentials
+WIFI_SSID = "<your wifi ssid>"
+WIFI_PW = "<your wifi password>"
+WIFI_RETRIES = 10
+
